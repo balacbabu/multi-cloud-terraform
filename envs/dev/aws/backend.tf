df11eaa14730bdbb82455a89e8ee6d31367acdc9
Balaji Chandrababu <balajicbabu@Balajis-MBP.Home>
2025-06-18 16:02:37 +0100

    formatting

--- envs/dev/aws/backend.tf
+++ envs/dev/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-balaji"  # replace with your bucket
+    bucket         = "terraform-state-balaji" # replace with your bucket
     key            = "dev/aws/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-locks"
